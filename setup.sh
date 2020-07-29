2fc9a2f57c25d2f3a9497c8ac0955c02c15a77b4
takhmina-tem <68912405+takhmina-tem@users.noreply.github.com>
2020-07-29 17:24:59 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 mkdir -p ~/.streamlit/
 echo “\
 [general]\n\
-email = \”your-email@domain.com\”\n\
-“ > ~/.streamlit/credentials.toml
+email = \”temirbay.takhmina@gmail.com\”\n\
+“ > ~/.streamlit/covidSt.py
 echo “\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
-“ > ~/.streamlit/config.toml
+“ > ~/.streamlit/covidSt.py
